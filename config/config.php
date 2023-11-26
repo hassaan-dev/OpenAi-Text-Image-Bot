c39b34548dc056aa84b0365d1281b366cd0409ad
Muhammad Hassaan <34338953+hassaan-dev@users.noreply.github.com>
2023-11-26 21:13:11 +0500

    refactor: backend code

--- config/config.php
+++ config/config.php
@@ -2,5 +2,5 @@
 
 return [
     // Replace 'your-api-key' with your actual OpenAI API key
-    'apiKey' => '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',
+    'apiKey' => 'your-api-key',
 ];
