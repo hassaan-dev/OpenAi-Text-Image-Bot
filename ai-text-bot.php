ea279a45b028581005e79f28099a32853765fa2a
Muhammad Hassaan <34338953+hassaan-dev@users.noreply.github.com>
2023-11-26 21:20:49 +0500

    refactor: AI Chat Bot

--- ai-text-bot.php
+++ ai-text-bot.php
@@ -38,15 +38,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <style>
         body {
             font-family: 'Arial', sans-serif;
-            /*background-color: #f8f9fa;*/
             margin: 0;
             padding: 0;
         }
-
-        /*#chat-container {
-            max-width: 800px;
-            margin: 0 auto;
-        }*/
 
         #chat-log {
             padding: 15px;
@@ -73,23 +67,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             align-self: flex-start;
             max-width: 70%;
         }
-
-        /*.user-message {
-            background-color: #007bff;
-            color: #fff;
-            align-self: flex-end;
-        }
-
-        .bot-message {
-            background-color: #28a745;
-            color: #fff;
-            align-self: flex-start;
-        }
-        .error-message {
-             background-color: #dc3545;
-             color: #fff;
-             align-self: flex-start;
-         }*/
 
         #user-input {
             display: flex;
@@ -122,21 +99,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             left: 10px;
             top: 10px;
             z-index: 1000;
-        }
-
-        .placeholder-spinner {
-            width: 3rem;
-            height: 3rem;
-            border: 0.25em solid currentColor;
-            border-top: 0.25em solid transparent;
-            border-radius: 50%;
-            animation: spinner-border 0.75s linear infinite;
-        }
-
-        @keyframes spinner-border {
-            to {
-                transform: rotate(360deg);
-            }
         }
     </style>
 </head>
@@ -177,11 +139,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     function sendMessage() {
         const userInput = document.getElementById('user-input-text');
         const userInputText = userInput.value;
-
-        // Display user's message immediately
-        // document.getElementById('chat-log').innerHTML += `<div class="d-flex justify-content-end"><div class="message user-message bg-primary text-white mb-2 p-2 rounded"><strong>User: </strong> ${userInput}</div></div>`;
-
         const chatLog = document.getElementById('chat-log');
+
+        // Add user message to the chat log
         chatLog.innerHTML += `<div class="d-flex justify-content-end"><div class="message user-message bg-primary text-white mb-2 p-2 rounded">${userInputText}</div></div>`;
 
         // Add a placeholder message to the chat log
@@ -192,19 +152,12 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         userInput.disabled = true;
         document.querySelector('button').disabled = true;
 
-        // Show loading message
-        // document.getElementById('loading-message').style.display = 'block';
-        // document.getElementById('loading-placeholder').style.display = 'block';
-
         // Make an AJAX request to the server
         const xhr = new XMLHttpRequest();
         xhr.open('POST', 'ai-text-bot.php', true);
         xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
         xhr.onreadystatechange = function () {
             if (xhr.readyState === 4) {
-                // Hide loading spinner
-                // document.getElementById('loading-message').style.display = 'none';
-                // document.getElementById('loading-placeholder').style.display = 'none';
 
                 // Remove the placeholder message
                 const placeholderMessage = chatLog.querySelector('.placeholder-glow');
@@ -219,51 +172,40 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                 if (xhr.status === 200) {
                     // Successful response
                     const response = JSON.parse(xhr.responseText);
-                    // Display bot message with enhanced styling
-                    // chatLog.innerHTML += `<div class="message bot-message bg-dark text-light float-start mb-2 p-2 rounded"><strong>Bot: </strong> ${response.response}</div>`;
 
-                    // const botReply = response.response.toString();
-                    // chatLog.innerHTML += `<div class="d-flex justify-content-start"><div class="message bot-message bg-dark text-light mb-2 p-2 rounded"><strong>Bot: </strong> <code>${botReply}</code></div></div>`;
-
+                    // preparing chat log message
                     const messageContent = document.createElement('div');
                     const classList = ['message', 'bot-message', 'bg-dark', 'text-light', 'mb-2', 'p-2', 'rounded', 'w-100'];
                     messageContent.classList.add(...classList);
 
                     if (isCode(response.response)) {
-                        // If the sender is the bot and the response is code-like, display as a code block
+                        // If the response is code-like, display as a code block
                         const codeBlock = document.createElement('pre');
                         const codeElement = document.createElement('code');
                         codeElement.innerText = response.response;
                         codeBlock.appendChild(codeElement);
-
                         messageContent.appendChild(codeBlock);
                     } else {
                         messageContent.innerText = response.response;
-                        // chatLog.innerHTML += `<div class="d-flex justify-content-start"><div class="message bot-message bg-dark text-light mb-2 p-2 rounded">${nl2br(response.response)}</div></div>`;
                     }
 
                     chatLog.innerHTML += `
                     <div class="d-flex justify-content-start">
                         ${messageContent.outerHTML}
                     </div>`;
-                    // chatLog.lastChild.appendChild(messageContent);
-                    // chatLog.innerHTML += `<div class="d-flex justify-content-start"><div class="message bot-message bg-dark text-light mb-2 p-2 rounded"><strong>Bot: </strong> `;
-                    // chatLog.innerText += response.response;
-                    // chatLog.innerHTML += `</div></div>`;
 
                     // Empty user input field after receiving a response
                     userInput.value = '';
 
                     // Focus on the user input field after receiving a response
                     userInput.focus();
-                    chatLog.scrollTop = chatLog.scrollHeight;
                 } else {
                     // Handle error response
                     const errorMessage = 'Error communicating with the server. Please try again.';
                     chatLog.innerHTML += `<div class="d-flex justify-content-start"><div class="message bg-danger text-light mb-2 p-2 rounded">${errorMessage}</div></div>`;
-                    chatLog.scrollTop = chatLog.scrollHeight;
                 }
-
+                // scroll to last chat log
+                chatLog.scrollTop = chatLog.scrollHeight;
             }
         };
         xhr.send(`user_input=${encodeURIComponent(userInputText)}`);
@@ -280,16 +222,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     }
 
     function startOver() {
-
         window.location.reload();
-
-        // Implement the logic to reset the chat or any other action needed
-        // For example, you might clear the chat log and input field
-        document.getElementById('chat-log').innerHTML = '';
-        document.getElementById('user-input-text').value = '';
-
-        // Optionally, you can also focus on the input field after starting over
-        document.getElementById('user-input-text').focus();
     }
 
     // Function to check if the response is code-like
--- config/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-return [
-    // Replace 'your-api-key' with your actual OpenAI API key
-    'apiKey' => 'your-api-key',
-];
